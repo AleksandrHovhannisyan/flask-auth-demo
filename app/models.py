6f00c5f7120329d74f818b6f4b4c207e0949989d
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2023-05-27 15:41:44 -0500

    Store salt in user table

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ class User(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String(64), index=True, unique=True)
     password_hash = database.Column(database.String(128))
+    password_salt = database.Column(database.String)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
